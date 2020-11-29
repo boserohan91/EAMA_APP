23d35f0c216f70885445fedcc85649ccaebb2be6
Rohan Bose <rohanbose@Rohans-MacBook-Air.local>
2020-11-29 12:22:08 +0100

    Group creation layout added

--- app/src/main/java/com/example/spontan/GroupCreation.java
+++ app/src/main/java/com/example/spontan/GroupCreation.java
@@ -1,14 +1,45 @@
 package com.example.spontan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
-public class GroupCreation extends AppCompatActivity {
+import java.text.DateFormat;
+import java.util.Calendar;
+
+public class GroupCreation extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_group_creation);
+
+        ImageButton dtPickerBtn = (ImageButton) findViewById(R.id.datePickerBtn);
+        dtPickerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment datePicker = new DatePickerFragment();
+                datePicker.show(getSupportFragmentManager(), "date_picker");
+            }
+        });
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.YEAR, year);
+        c.set(Calendar.MONTH, month);
+        c.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+        String currentDateString = DateFormat.getDateInstance(DateFormat.FULL).format(c.getTime());
+        TextView dttextView = (TextView) findViewById((R.id.dateText));
+        dttextView.setText(currentDateString);
+
     }
 }
--- app/src/main/java/com/example/spontan/SearchActivityFragment.java
+++ app/src/main/java/com/example/spontan/SearchActivityFragment.java
@@ -1,9 +1,11 @@
 package com.example.spontan;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -15,5 +17,17 @@ public class SearchActivityFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.search_activity, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        Button createGroupBtn = (Button) getView().findViewById(R.id.grpCreateBtn);
+        createGroupBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), GroupCreation.class);
+                startActivity(intent);
+            }
+        });
     }
 }
